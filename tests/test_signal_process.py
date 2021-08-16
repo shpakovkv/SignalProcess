358e693bc0b26a052124e33a1496b7a986e00838
Konstantin <konstantine.shpakov@gmail.com>
2021-08-16 16:01:06 +0300

    Started unittest branch.

---
+++ tests/test_signal_process.py
@@ -0,0 +1,55 @@
+# def test_test_get_parser():
+#
+#
+# def test_test_add_to_log(s, print_to_console=True):
+
+
+def test_smooth_voltage():
+    raise RuntimeError("Not implemented yet.")
+
+
+def test_align_and_append_ndarray():
+    raise RuntimeError("Not implemented yet.")
+
+
+def test_y_zero_offset(curve, start_x, stop_x):
+    raise RuntimeError("Not implemented yet.")
+
+
+def test_y_zero_offset_all(signals_data, curves_list, start_stop_tuples):
+    raise RuntimeError("Not implemented yet.")
+
+
+def test_pretty_print_nums(nums, prefix=None, postfix=None,
+                           s=u'{pref}{val:.2f}{postf}', show=True):
+    raise RuntimeError("Not implemented yet.")
+
+
+def test_raw_y_auto_zero(params, multiplier, delay):
+    raise RuntimeError("Not implemented yet.")
+
+
+def test_update_by_y_auto_zero(data, y_auto_zero_params,
+                               multiplier, delay, verbose=True):
+    raise RuntimeError("Not implemented yet.")
+
+
+def test_do_y_zero_offset(signals_data, cl_args):
+    raise RuntimeError("Not implemented yet.")
+
+
+def test_get_front_point(signals_data, args, multiplier, delay,
+                         front_plot_name, interactive=False):
+    raise RuntimeError("Not implemented yet.")
+
+
+def test_do_offset_by_front(signals_data, cl_args, shot_name):
+    raise RuntimeError("Not implemented yet.")
+
+
+def test_zero_one_curve(curve, max_rows=30):
+    raise RuntimeError("Not implemented yet.")
+
+
+def test_zero_curves(signals, curve_indexes, verbose=False, max_rows=30):
+    raise RuntimeError("Not implemented yet.")
